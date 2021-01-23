0d11a5e5f8a243f47e7faf98db5d72670e9d275e
Viktoriia Romaniuk <creative.mashine@gmail.com>
2021-01-23 23:59:02 +0200

    Custom Table View Cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Viktorija Romanjuk on 23.01.2021.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+
+    @IBOutlet var imageOfPlace: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    
+    
+    
+}
