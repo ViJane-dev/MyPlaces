8151e0a5324ec89f2ab24b6712c7315ed2fd2d14
Viktoriia Romaniuk <creative.mashine@gmail.com>
2021-01-27 04:03:45 +0200

    KeyBoard of TextFields Custom Cells

---
+++ MyPlaces/NewPlaceViewController.swift
@@ -0,0 +1,42 @@
+//
+//  NewPlaceViewController.swift
+//  MyPlaces
+//
+//  Created by Viktorija Romanjuk on 27.01.2021.
+//
+
+import UIKit
+
+class NewPlaceViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.tableFooterView = UIView()
+
+    }
+    
+    //MARK: Table view delegate
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 0 {
+            
+        } else {
+            view.endEditing(true)
+        }
+    }
+
+}
+
+
+//MARK: Text field delegate
+
+
+extension NewPlaceViewController: UITextFieldDelegate {
+    //скрываем клавиатуру по нажатию на Done
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+        textField.resignFirstResponder()
+        return true
+    }
+}
